f38f384bc923ab04f087cc6b62a5e1c744221432
pkondrashkov <pkondrashkov@gmail.com>
2021-07-22 17:09:08 +0300

    0.0.3

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "RegulaCommon",
-            url: "https://pods.regulaforensics.com/RegulaCommon/regula-common/0.0.2/RegulaCommon.xcframework.zip",
-            checksum: "b353f1e51611c8bbbca40b5836caec9ae63615aa210f25cee64faca6dec77d6d"
+            url: "https://pods.regulaforensics.com/RegulaCommon/regula-common/0.0.3/RegulaCommon.xcframework.zip",
+            checksum: "5e14a8f082cb6d327c9d679970576e9f8e28d1e6ca6b1b09befb6bc4e5d0b7f0"
         )
     ]
 )
